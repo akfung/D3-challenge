7c8e9d6d314f708f81632315c8b55ba6919a597c
Amos Fung <amosfung@amoss-mbp.home>
2020-03-24 01:56:56 -0700

    started multiple axis features

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,7 +6,7 @@ let svgHeight = 700,
 // make responsive function
 function makeResponsive() {
 
-  var svgArea = d3.select("body").select("svg");
+  var svgArea = d3.select("#scatter").select("svg");
   if (!svgArea.empty()) {
     svgArea.remove();
   }}
@@ -32,43 +32,61 @@ let svg = d3.select('#scatter')
 let chartGroup = svg.append('g')
     .attr('transform', `translate(${chartMargins.left}, ${chartMargins.top})`);
 
-
-
 // read csv and draw
 let csvPath = './assets/data/data.csv';
 d3.csv(csvPath).then(csvData => {
     // test to print data
     console.log(csvData);
 
-    // forEach to convert string to int
+    // forEach to convert column values to int
     csvData.forEach(function(state) {
-        state['poverty'] = +state['poverty'];
-        state['healthcare'] = +state['healthcare'];
-        state['obesity'] = +state['obesity'];
+        state.poverty = +state.poverty;
+        state.healthcare = +state.healthcare;
+        state.obesity = +state.obesity;
+        state.age = +state.age;
+        state.smokes = +state.smokes;
+        state.income = +state.income;
     });
     
-    // set x and y scales for axes
-    let yScale = d3.scaleLinear()
-    .domain([0, d3.max(csvData, d => d.poverty)])
-    .range([chartHeight, 0]);
-
-    let xScale = d3.scaleLinear()
+    // set x scales for axes
+    let healthcareXScale = d3.scaleLinear()
     .domain([0, d3.max(csvData, d => d.healthcare)])
     .range([0, chartWidth]);
 
+    let ageXScale = d3.scaleLinear()
+    .domain([0, d3.max(csvData, d => d.age)])
+    .range([0, chartWidth]);
+
+    let incomeXScale = d3.scaleLinear()
+    .domain([0, d3.max(csvData, d => d.income)])
+    .range([0, chartWidth]);
+
+    // set y scales for axes
+    let povertyYScale = d3.scaleLinear()
+    .domain([0, d3.max(csvData, d => d.poverty)])
+    .range([chartHeight, 0]);
+
+    let smokesYScale = d3.scaleLinear()
+    .domain([0, d3.max(csvData, d => d.smokes)])
+    .range([chartHeight, 0]);
+
+    let obesityYScale = d3.scaleLinear()
+    .domain([0, d3.max(csvData, d => d.obesity)])
+    .range([chartHeight, 0]);
+    
     // create Axes
-    let bottomAxis = d3.axisBottom(xScale);
-    let leftAxis = d3.axisLeft(yScale);
+    let bottomAxisHealthcare = d3.axisBottom(healthcareXScale);
+    let leftAxisPoverty = d3.axisLeft(povertyYScale);
 
     //append axes to svg group
     //left
     chartGroup.append('g')
-        .call(leftAxis);
+        .call(leftAxisPoverty);
 
     //bottom
     chartGroup.append('g')
         .attr('transform', `translate(0, ${chartHeight})`)
-        .call(bottomAxis);
+        .call(bottomAxisHealthcare);
 
     // BIG MONEY TIME MAKE THE CIRCLES
     let stateCircles = chartGroup.selectAll('.stateCircle')
@@ -76,8 +94,8 @@ d3.csv(csvPath).then(csvData => {
         .enter()
         .append('circle')
         .classed('stateCircle', true)
-        .attr('cx', d => xScale(d.healthcare))
-        .attr('cy', d => yScale(d.poverty))
+        .attr('cx', d => healthcareXScale(d.healthcare))
+        .attr('cy', d => povertyYScale(d.poverty))
         .attr('r', '10');
 
     // add circle text for each state
@@ -87,24 +105,48 @@ d3.csv(csvPath).then(csvData => {
         .append('text')
         .text(d => d.abbr)
         .classed('stateText', true)
-        .attr('x', d => xScale(d.healthcare))
-        .attr('y', d => yScale(d.poverty))
+        .attr('x', d => healthcareXScale(d.healthcare))
+        .attr('y', d => povertyYScale(d.poverty))
         .attr('font-size', '10px');
 
     //draw axis labels
-    // x axis label
+    // x axis labels
     chartGroup.append('text')
         .attr('transform', `translate(${chartWidth/2}, ${chartHeight + chartMargins.top - 50})`)
         .attr('text-anchor', 'middle')
         .attr('font-size', '16px')
         .text('% of Households with Healthcare');
+    
+    chartGroup.append('text')
+        .attr('transform', `translate(${chartWidth/2}, ${chartHeight + chartMargins.top - 30})`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '16px')
+        .text('Average Household Age');
 
-    // y axis label
+    chartGroup.append('text')
+        .attr('transform', `translate(${chartWidth/2}, ${chartHeight + chartMargins.top - 10})`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '16px')
+        .text('Average Household Income');
+
+    // y axis labels
     chartGroup.append('text')
         .attr('transform', `translate(${chartMargins.left - 150}, ${chartHeight/2}) rotate(-90)`)
         .attr('text-anchor', 'middle')
         .attr('font-size', '16px')
         .text('% of Households with an Obese Adult');
+
+    chartGroup.append('text')
+        .attr('transform', `translate(${chartMargins.left - 170}, ${chartHeight/2}) rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '16px')
+        .text('% of Households in Poverty');
+
+    chartGroup.append('text')
+        .attr('transform', `translate(${chartMargins.left - 190}, ${chartHeight/2}) rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '16px')
+        .text('% of Households with Smokers');
 
     // Let's add tooltips - fun for the whole family
     // create tooltip and call to svg chart area
@@ -112,9 +154,9 @@ d3.csv(csvPath).then(csvData => {
         .attr('class', 'd3-tip')
         .offset([120,80])
         .html(function(d) {
-            return (`<p><strong>${d['state']}</strong></p>
-            <p>${d['healthcare']}% have healthcare</p>
-            <p>${d['poverty']}% in poverty</p>`);
+            return (`<p><strong>${d.state}</strong></p>
+            <p>${d.healthcare}% have healthcare</p>
+            <p>${d.poverty}% in poverty</p>`);
         });
 
     chartGroup.call(toolTip);
@@ -125,6 +167,7 @@ d3.csv(csvPath).then(csvData => {
     })
     .on('mouseout', function(d){
         toolTip.hide(d);
+
     });
 
     
@@ -134,5 +177,7 @@ d3.csv(csvPath).then(csvData => {
         console.log(error);
     });
     
-    makeResponsive();
-    d3.select(window).on("resize", makeResponsive);
+// makeResponsive();
+// d3.select(window).on("resize", makeResponsive);
+
+
